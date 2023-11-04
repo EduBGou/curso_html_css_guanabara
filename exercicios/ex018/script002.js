bfd277e8b57da67b33a3009bb0e5f5ffb95889d2
EduBGou <eduardotexugo@gmail.com>
2023-11-04 15:50:03 -0300

    javaScript

---
+++ exercicios/ex018/script002.js
@@ -0,0 +1,17 @@
+function sumTotalPopulation(){
+    let res = document.getElementById('res')
+    let lNum = [44411238, 20538718, 16054524, 14141626, 11444380]
+    let total = 0
+    for(let i = 0; i < lNum.length; i++){
+        total += lNum[i]
+    }
+    total = String(total)
+    for(let i = total.length; i > 0; i--){
+        res.innerHTML += total[i - 1]
+        if(i % 3 == 1 && i != 1){
+            res.innerHTML += ' '
+        }
+    }
+    
+    
+}
